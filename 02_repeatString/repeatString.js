c319d617f2237a097c752e5f349299a17adb6cff
Predator654 <piotr.budzynski@protonmail.com>
2023-11-28 23:29:23 +0100

    Exes complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
 
+
+const repeatString = function(x, y) {
+    const string = 'hey';
+    for (let num = 1; num <= 3; num++) {
+        return string;
+}
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
 
-};
+
+const reverseString = function(string) {
+        
+    let straightArray = Array.from(string);
+    let reversedArray = straightArray.reverse();
+    return reversedString = reversedArray.join('');
+   /* let length = liminalArray.length;
+
+    for (let i = 0; i < length - 2; i++) {
+    return liminalArray[i] = liminalArray.pop();
+} */
+}
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
